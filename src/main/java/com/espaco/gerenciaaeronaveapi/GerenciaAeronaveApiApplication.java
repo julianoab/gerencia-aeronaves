f21fa6291bd1411c73c6d21056f53cc18f65d576
julianoab <julianoasud@hotmail.com>
2022-09-02 11:06:04 -0300

    Projeto criado

---
+++ src/main/java/com/espaco/gerenciaaeronaveapi/GerenciaAeronaveApiApplication.java
@@ -0,0 +1,13 @@
+package com.espaco.gerenciaaeronaveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaAeronaveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaAeronaveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espaco/gerenciaaeronaveapi/GerenciaAeronaveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espaco.gerenciaaeronaveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaAeronaveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
